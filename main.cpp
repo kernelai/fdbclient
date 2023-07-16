d7eb002c777092927916992e9051ed68302a6384
kernelai <redtomato158@gmail.com>
2023-07-16 15:04:53 +0000

    format: format main.cc
    Signed-off-by: kernelai <redtomato158@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #define FDB_API_VERSION 710
 #include <foundationdb/fdb_c.h>
 
-int main() { std::cout << "hello,world!" << std::endl;
-    fdb_select_api_version(FDB_API_VERSION);
- }
+int main() {
+  std::cout << "hello,world!" << std::endl;
+  fdb_select_api_version(FDB_API_VERSION);
+}
