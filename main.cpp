6bcca505098bc6c6fea79515609036be440c5cf9
kernelai <redtomato158@gmail.com>
2023-07-16 06:12:23 +0000

    debug: findpackage can't output include dir
    Signed-off-by: kernelai <redtomato158@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
-int main(){
-    std::cout << "hello,world!" << std::endl;
-}
+#define FDB_API_VERSION 710
+#include <foundationdb/fdb_c.h>
+
+int main() { std::cout << "hello,world!" << std::endl;
+    fdb_select_api_version(FDB_API_VERSION);
+ }
